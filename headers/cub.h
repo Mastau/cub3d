d24e4101e93396d944ba34673df99acc0da14692
Thomas Arnaud <thomarna@student.42angouleme.fr>
2025-03-11 15:41:03 +0100

    Create Header (cub.h) > Define struct for map

---
+++ headers/cub.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thomarna <thomarna@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 15:34:21 by thomarna          #+#    #+#             */
+/*   Updated: 2025/03/11 15:40:38 by thomarna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB_H
+# define CUB_H
+
+# ifndef WIDTH
+#  define WIDTH 1920
+# endif
+
+# ifndef HEIGHT
+#  define HEIGHT 1080
+# endif
+
+typedef struct s_cub {
+	char **map;
+	char *no;
+	char *so;
+	char *we;
+	char *ea;
+	char *floor;
+	char *ceiling;
+
+} t_cub;
+
+typedef struct s_mlx {
+	void	*con;
+	void	*win;
+} t_mlx;
+
+#endif

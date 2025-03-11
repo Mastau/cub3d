d6aa7fee62430b701115f96dce8ce9bcff5a0fec
Jérémy Lorette <jeremy.lorette@outlook.com>
2025-03-11 19:12:50 +0100

    create fake data and custom makefile

---
+++ fakedata/fake_data.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fake_data.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/11 16:49:52 by jlorette          #+#    #+#             */
+/*   Updated: 2025/03/11 19:10:39 by jlorette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <libft.h>
+
+t_cub   init_fake_data(void)
+{
+	t_cub   fake;
+	char    **map;
+
+	map = lp_alloc(sizeof(char *) * 15);
+	fake.no = lp_alloc(sizeof(char) * 19);
+	fake.so = lp_alloc(sizeof(char) * 19);
+	fake.we = lp_alloc(sizeof(char) * 19);
+	fake.ea = lp_alloc(sizeof(char) * 19);
+	fake.floor = lp_alloc(sizeof(char) * 10);
+	fake.ceiling = lp_alloc(sizeof(char) * 9);
+
+	ft_strlcpy(fake.no, "../textures/no.png", 19);
+	ft_strlcpy(fake.so, "../textures/so.png", 19);
+	ft_strlcpy(fake.we, "../textures/we.png", 19);
+	ft_strlcpy(fake.ea, "../textures/ea.png", 19);
+	ft_strlcpy(fake.floor, "220,100,0", 10);
+	ft_strlcpy(fake.ceiling, "225,30,0", 9);
+
+	map[0] = ft_strdup("        1111111111111111111111111");
+	map[1] = ft_strdup("        1000000000110000000000001");
+	map[2] = ft_strdup("        1011000001110000000000001");
+	map[3] = ft_strdup("        1001000000000000000000001");
+	map[4] = ft_strdup("111111111011000001110000000000001");
+	map[5] = ft_strdup("100000000011000001110111111111111");
+	map[6] = ft_strdup("11110111111111011100000010001");
+	map[7] = ft_strdup("11110111111111011101010010001");
+	map[8] = ft_strdup("11000000110101011100000010001");
+	map[9] = ft_strdup("10000000000000001100000010001");
+	map[10] = ft_strdup("10000000000000001101010010001");
+	map[11] = ft_strdup("11000001110101011111011110N0111");
+	map[12] = ft_strdup("11110111 1110101 101111010001");
+	map[13] = ft_strdup("11111111 1111111 111111111111");
+	map[14] = NULL;
+
+	fake.map = map;
+	return (fake);
+}
+
+void print_data(t_cub data)
+{
+	int i;
+
+	printf("NO: %s\n", data.no);
+	printf("SO: %s\n", data.so);
+	printf("WE: %s\n", data.we);
+	printf("EA: %s\n", data.ea);
+	printf("Floor: %s\n", data.floor);
+	printf("Ceiling: %s\n", data.ceiling);
+	printf("Map:\n");
+	i = 0;
+	while (data.map[i])
+	{
+		printf("%s\n", data.map[i]);
+		i++;
+	}
+}
--- headers/debug.h
+++ headers/debug.h
@@ -1,42 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub.h                                              :+:      :+:    :+:   */
+/*   debug.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlorette <jlorette@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/11 15:34:21 by thomarna          #+#    #+#             */
-/*   Updated: 2025/03/11 18:45:08 by jlorette         ###   ########.fr       */
+/*   Created: 2025/03/11 18:08:27 by jlorette          #+#    #+#             */
+/*   Updated: 2025/03/11 18:14:43 by jlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB_H
-# define CUB_H
+#ifndef DEBUG_H
+#define DEBUG_H
 
-# ifndef WIDTH
-#  define WIDTH 1920
-# endif
+#include <cub.h>
 
-# ifndef HEIGHT
-#  define HEIGHT 1080
-# endif
-
-typedef struct s_cub
-{
-	char **map;
-	char *no;
-	char *so;
-	char *we;
-	char *ea;
-	char *floor;
-	char *ceiling;
-
-}	t_cub;
-
-typedef struct s_mlx
-{
-	void	*con;
-	void	*win;
-}	t_mlx;
+t_cub init_fake_data(void);
+void print_data(t_cub data);
 
 #endif
